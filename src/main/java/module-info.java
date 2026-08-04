57ec9298df0965d40497e0b006150d93abb7a5fa
Yashgamer-x <yashdesai121212@gmail.com>
2026-08-04 10:44:01 -0400

    Add Ikonli Core and FontAwesome6 dependencies to improve icon support

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module org.yashgamerx.radar {
     requires javafx.fxml;
     requires org.kordamp.ikonli.core;
     requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.fontawesome6;
 
     opens org.yashgamerx.radar to javafx.fxml;
     exports org.yashgamerx.radar;
