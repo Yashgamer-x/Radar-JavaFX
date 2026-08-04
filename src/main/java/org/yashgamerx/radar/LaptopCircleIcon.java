68c918d2d3df74876dc968746b66dd16280fa2d2
Yashgamer-x <yashdesai121212@gmail.com>
2026-08-04 11:09:09 -0400

    Add drop shadow effect to LaptopCircleIcon for improved visual depth

--- src/main/java/org/yashgamerx/radar/LaptopCircleIcon.java
+++ src/main/java/org/yashgamerx/radar/LaptopCircleIcon.java
@@ -1,6 +1,7 @@
 package org.yashgamerx.radar;
 
 import javafx.beans.property.DoubleProperty;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -13,8 +14,17 @@ public class LaptopCircleIcon extends StackPane {
 
     public LaptopCircleIcon() {
         circle.setFill(Color.DODGERBLUE);
+
+        DropShadow shadow = new DropShadow();
+        shadow.setColor(Color.rgb(0, 0, 0, 0.35));
+        shadow.setRadius(12);
+        shadow.setOffsetY(3);
+        shadow.setSpread(0.15);
+        circle.setEffect(shadow);
+
         icon.setIconColor(Color.WHITE);
         icon.iconSizeProperty().bind(circle.radiusProperty().multiply(0.5));
+
         getChildren().addAll(circle, icon);
     }
 
