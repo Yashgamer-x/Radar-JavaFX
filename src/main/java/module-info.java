8e511613559714db621d762666c09fa2a0477590
Yashgamer-x <yashdesai121212@gmail.com>
2026-08-04 10:31:31 -0400

    Add Ikonli dependency and Maven Shade plugin for JavaFX improvements

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module org.yashgamerx.radar {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires org.kordamp.ikonli.core;
+    requires org.kordamp.ikonli.javafx;
 
     opens org.yashgamerx.radar to javafx.fxml;
     exports org.yashgamerx.radar;
